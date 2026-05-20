782d612161772b143addaa5db6a4d81aad45c78e
Sophia <sophia28.soares@gmail.com>
2026-05-20 17:14:02 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -2,16 +2,16 @@ import streamlit as st
 import pandas as pd
 
 ### 1. Leia o Arquivo Dados_Artistas.csv e o Transforme em dataframe
-
+df = pd.read_parquet("Dados_Artistas.parquet")
 
 ### 2. Coloque um titulo na pagina
-
+st.title ("MUSIC SHOW 🎶🎧")
 
 ### 3. Coloque subtitulo titulo na pagina
-
+st.subheader("Escute suas musicas favoritas e apenas aproveite")
 
 ### 4. Coloque uma logo na sidebar(barra lateral)
-
+st.sidebar.image("logo.png")
 
 ### 5. Não mexa abaixo, estamos criando uma selectbox, para selecionar o artista
 artistas = st.sidebar.selectbox('Selecione o Artista', df['Artist'].unique())
@@ -32,4 +32,5 @@ for index, row in df_artista.iterrows():
             
             st.video(row['Url_youtube'])
             st.markdown("---")
-st.link_button('Ouça no Spotify', url=row['Url_spotify'], type='primary')
+st.link_button('Ouça no Spotify', url=row['Url_spotify'], type='primary') 
+
